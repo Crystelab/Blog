f99e3d664b06ebb451a9e392921dd484fe6b8f39
Crystel Abou-Nahed <crystelabounahed@hotmail.com>
2024-11-03 17:51:39 -0500

    Fix: Change page underline

--- public/js/tags.js
+++ public/js/tags.js
@@ -1,11 +1,17 @@
-fetch('html/tags.html')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.text();
-            })
-            .then(data => {
-                document.getElementById('tags').innerHTML = data;
-            })
-            .catch(error => console.error('Error loading the header:', error));
+function createTags() {
+    const tagsSection = document.createElement('div'); // Create a container div for the tags
+    tagsSection.innerHTML = `
+        <br>
+        <h3>Explore by tags</h3>
+        <ul class="tags">
+            <li><a id="learning">#learning</a></li>
+            <li><a id="technical">#technical</a></li>
+            <li><a id="meta">#meta</a></li>
+            <li><a id="personal">#personal</a></li>
+        </ul>
+        <br>
+    `;
+    document.getElementById('tags').prepend(tagsSection); // Prepend the tags section to the element with id 'tags'
+}
+
+document.addEventListener('DOMContentLoaded', createTags);
--- public/js/top.js
+++ public/js/top.js
@@ -1,11 +1,32 @@
-fetch('html/top.html')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.text();
-            })
-            .then(data => {
-                document.getElementById('top').innerHTML = data;
-            })
-            .catch(error => console.error('Error loading the header:', error));
+function createNavbar() {
+    const navbar = document.createElement('div');
+    navbar.innerHTML = `
+        <header>
+            <h1>Crystel Abou-Nahed</h1>
+            <h2>Software Engineering Student</h2>
+        </header>
+
+       <nav>
+            <a href="/" class="nav-link active">Home</a>
+            <a href="/posts" class="nav-link">Posts</a>
+            <a href="/minigame" class="nav-link">Mini Game</a>
+            <a href="https://github.com/Crystelab" class="nav-link">GitHub</a>
+            <a href="https://www.linkedin.com/in/crystel-abou-nahed-7216272ab/" class="nav-link">LinkedIn</a>
+        </nav>
+    `;
+    document.getElementById('top').prepend(navbar);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    createNavbar();
+
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
