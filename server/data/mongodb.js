c9943c86c6bebfe19bc4d0ad974e5aa9a2fef213
Crystelab <crystelabounahed@hotmail.com>
2024-11-07 14:10:14 -0500

    Try this

--- server/data/mongodb.js
+++ server/data/mongodb.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 const connectDB = async () => {
-  
   try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected: ${conn.connection.host}`);
+      mongoose.set('strictQuery', false);
+      const conn = await mongoose.connect(process.env.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+      });
+      console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+      console.error('MongoDB connection error:', error);
+      process.exit(1);
   }
 }
-
 module.exports = connectDB;
